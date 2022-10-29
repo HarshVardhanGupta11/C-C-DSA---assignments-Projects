1f8fbc76df9585a19cf570991612456b369c8e32
HarshVardhanGupta11 <115100422+HarshVardhanGupta11@users.noreply.github.com>
2022-10-29 11:36:22 +0530

    Q10.c

--- Assignment-2/Q10.c
+++ Assignment-2/Q10.c
@@ -3,7 +3,7 @@ int main()
 {
     int h,m;
     printf("Enter hour and minute\n");
-    scanf("%d:%d",&h,&m);
+    scanf("%d:%d",&h,&m);  // : is the delimiter
     printf("%d hour and %d minute",h,m);
     return 0;
 }
