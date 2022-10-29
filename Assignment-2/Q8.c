fdcb028ca01994d55c5fae0039649c330f23d8fd
HarshVardhanGupta11 <115100422+HarshVardhanGupta11@users.noreply.github.com>
2022-10-29 11:38:24 +0530

    Q8.c

--- Assignment-2/Q8.c
+++ Assignment-2/Q8.c
@@ -3,7 +3,7 @@ int main()
 {
     char ch1,ch2,ch3;
     printf("Enter three characters\n");
-    scanf("%c%c%c",&ch1,&ch2,&ch3);
+    scanf("%c %c %c",&ch1,&ch2,&ch3); //space,\n(new line/enter),\t(tab) all can be taken as delimiter
     printf("%c=%d %c=%d %c=%d",ch1,ch1,ch2,ch2,ch3,ch3);
     return 0;
 }
