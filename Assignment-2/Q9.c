d2925cce951ba857ef81077a6189943c2052e9de
HarshVardhanGupta11 <115100422+HarshVardhanGupta11@users.noreply.github.com>
2022-10-29 11:39:52 +0530

    Q9.c

--- Assignment-2/Q9.c
+++ Assignment-2/Q9.c
@@ -3,7 +3,7 @@ int main()
 {
     int d,m,y;
     printf("Enter day month and year\n");
-    scanf("%d%d%d",&d,&m,&y);
+    scanf("%d/%d/%d",&d,&m,&y); //here / is the delimiter
     printf("Day - %d,Month - %d,Year - %d",d,m,y);
     return 0;
 }
